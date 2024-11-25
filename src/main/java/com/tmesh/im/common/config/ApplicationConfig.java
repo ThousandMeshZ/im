bc7e2dcac8c0ca3762da20a7e7781f6bfb8cd867
zhl <1009191578@qq.com>
2024-11-25 09:34:26 +0800

    cors过滤器修改

--- src/main/java/com/tmesh/im/common/config/ApplicationConfig.java
+++ src/main/java/com/tmesh/im/common/config/ApplicationConfig.java
@@ -103,13 +103,20 @@ public class ApplicationConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         if (YesOrNoEnum.YES.getCode().equals(cors)) {
-            CorsConfiguration corsConfiguration = new CorsConfiguration();
-            corsConfiguration.addAllowedOrigin("*");
-            corsConfiguration.addAllowedHeader("*");
-            corsConfiguration.addAllowedMethod("*");
-            corsConfiguration.setAllowCredentials(true);
-            source.registerCorsConfiguration("/**", corsConfiguration);
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowCredentials(true);
+            // 设置访问源地址，（*）表示匹配所有。
+            config.addAllowedOriginPattern("*");
+            // 设置访问源请求头
+            config.addAllowedHeader("*");
+            // 设置访问源请求方法
+            config.addAllowedMethod("*");
+            // 有效期 1800秒，设置预检请求（OPTIONS请求）的缓存时间，1800秒意味着客户端可以缓存这个设置1800秒
+            config.setMaxAge(3600L);
+            // 添加映射路径，拦截一切请求
+            source.registerCorsConfiguration("/**", config);
         }
+        // 返回新的CorsFilter
         return new CorsFilter(source);
     }
 }
